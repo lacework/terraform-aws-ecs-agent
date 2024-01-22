ed6e0003d0d8d530360d55d1e1fd84c89fd51def
Darren Murray <darren.murray@lacework.net>
2024-01-22 15:02:17 +0000

    chore: add lacework_metric_module datasource
    Signed-off-by: Darren Murray <darren.murray@lacework.net>

--- versions.tf
+++ versions.tf
@@ -1,7 +1,10 @@
 terraform {
   required_version = ">= 0.14"
   
-  required_providers {    
+  required_providers {   
+    aws    = ">= 5.0" 
+    random = ">= 2.1"
+    
     lacework = {
       source  = "lacework/lacework"
       version = "~> 1.18"
